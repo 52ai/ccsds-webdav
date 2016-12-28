b6b1b589e0270950ee6d690931d174042506ce20
52ai <ieeflsyu@outlook.com>
2016-12-28 15:45:56 +0800

    commit ccsds-webdav

--- test_easywebdav.py
+++ test_easywebdav.py
@@ -4,7 +4,7 @@ import os
 # Start off by creating a client object. Username and
 # password may be omitted if no authentication is needed.
 # webdav = easywebdav.connect('192.168.102.205, username='nssc', password='123456',protocol='https', port=443, verify_ssl=False)
-webdav = easywebdav.connect('192.168.102.205', username='nssc', password='123456',protocol='http', port=80, verify_ssl=False)
+webdav = easywebdav.connect('localhost', username='nssc', password='123456',protocol='http', port=80, verify_ssl=False)
 # Do some stuff:
 #webdav.mkdir('some_dir')
 #webdav.rmdir('another_dir')
